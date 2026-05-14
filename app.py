c684f5aaeef2802805cb9c31f428def2252a8a3a
jasons2 <jasons2@cisco.com>
2026-05-14 19:33:41 +0000

    Added Sample Functions

--- app.py
+++ app.py
@@ -1,63 +1,75 @@
 from creds import client_id, client_secret
 import requests
 
-# Get Access Token
-url = "https://id.cisco.com/oauth2/default/v1/token"
+def get_hardware(bearer_token: str, customer_id: str) -> list:
+    url = f"https://apix.cisco.com/cs/api/v2/inventory/hardware?customerId={customer_id}"
 
-# The data dictionary handles the application/x-www-form-urlencoded encoding
-payload = {
-    'grant_type': 'client_credentials',
-    'client_id': client_id,
-    'client_secret': client_secret
-}
+    payload = None
 
-try:
-    response = requests.post(url, data=payload)
-    
-    # Raise an exception for bad status codes (4xx or 5xx)
-    response.raise_for_status()
-    
-    # Print the JSON response
-    print(response.json())
+    headers = { "Accept": "application/json" }
 
-except requests.exceptions.RequestException as e:
-    print(f"An error occurred: {e}")
+    response = requests.request('GET', 
+                                url, 
+                                headers=headers, 
+                                data = payload)
 
-bearer_token = response.json()['access_token']
-
-### Get Customer Details
-
-url = "https://apix.cisco.com/cs/api/v2/customer-info/customer-details"
-
-# Define the headers
-headers = {
-    "Accept": "application/json",
-    'Authorization': 'Bearer {bearer_token}'
-}
-
-try:
-    # Perform the GET request
-    response = requests.get(url, headers=headers)
-    
-    # Check for HTTP errors
-    response.raise_for_status()
-
-except requests.exceptions.RequestException as e:
-    print(f"An error occurred: {e}")
+    return response.text.encode('utf8') 
 
 
-import sys
-sys.exit()
+def get_customer_details(bearer_token: str) -> str:
+    ### Get Customer Details
 
-#### Make Request
+    url = "https://apix.cisco.com/cs/api/v2/customer-info/customer-details"
 
-url = "https://apix.cisco.com/cs/api/v2/inventory/hardware?customerId="
+    # Define the headers
+    headers = {
+        "Accept": "application/json",
+        'Authorization': 'Bearer {bearer_token}'
+    }
 
-payload = None
+    try:
+        # Perform the GET request
+        response = requests.get(url, 
+                                headers=headers)
+        
+        # Check for HTTP errors
+        response.raise_for_status()
 
-headers = { "Accept": "application/json" }
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
 
-response = requests.request('GET', url, headers=headers, data = payload)
+    return response.text.encode('utf8')
 
-print(response.text.encode('utf8'))
 
+def get_bearer_token(client_id: str, client_secret: str) -> str:
+
+    url = "https://id.cisco.com/oauth2/default/v1/token"
+
+    # The data dictionary handles the application/x-www-form-urlencoded encoding
+    payload = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret
+    }
+
+    try:
+        response = requests.post(url, 
+                                 data=payload)
+        
+        # Raise an exception for bad status codes (4xx or 5xx)
+        response.raise_for_status()
+        
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+    return response.json()['access_token']
+
+
+def main():
+    bearer_token= get_bearer_token(client_id, client_secret)
+
+    # Print results from get customer details function
+    print(get_customer_details(bearer_token))
+
+if __name__ == "__main__":
+    main()
